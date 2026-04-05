12ede10eba1880ef6cc47fed684b5dde5129b5f4
JosephBull7 <143613327+JosephBull7@users.noreply.github.com>
2026-04-05 16:06:56 -0400

    Add LW/SW

--- sourcecode/data_memory.v
+++ sourcecode/data_memory.v
@@ -28,6 +28,23 @@ module data_memory(
     input [15:0] write_data,
     output reg [15:0] read_data
     );
+reg [7:0] memory [0:255];
+
+//store word
+always @(posedge clock) begin
+    if (mem_write) begin
+        memory[address]     <= write_data[15:8]; // MSB
+        memory[address + 1] <= write_data[7:0];  // LSB
+    end
+end
+
+//load word
+always @(*) begin
+    if (mem_read)
+        read_data = {memory[address], memory[address+1]};
+    else
+        read_data = 16'b0;
+end
 
   
 
